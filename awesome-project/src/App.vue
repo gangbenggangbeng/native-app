0e19864e2a5258eaacd1f3f1dcfda3a203755965
gege <2219022636@qq.com>
2017-09-15 09:41:36 +0800

    tijao-nativ-app

---
+++ awesome-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app">
+    <!-- <image class="img" :src="src"></image> -->
+    <hello></hello>
+    <foo></foo>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+import Foo from "./components/foo.vue"
+export default {
+  name: 'app',
+  data () {
+    return {
+      src: "https://img.alicdn.com/tfs/TB1OOl1SVXXXXc_XVXXXXXXXXXX-340-340.png"
+    }
+  },
+  components: {
+    Hello,
+    Foo
+  }
+}
+</script>
+
+<style scoped>
+.app {
+  align-items: center;
+  padding-top: 200px;
+}
+.img {
+  width: 340px;
+  height: 340px;
+}
+</style>
---
+++ awesome-project/src/components/foo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="wrapper">
+        <text class="h1">{{ msg }}</text>
+        <text class="weex">hello weex!!</text>
+        <text class="vue">我是庞格格</text>
+
+        
+
+
+    </div>
+</template>
+<style scoped>
+html{ 
+    scrollbar-face-color:#f6f6f6; 
+    scrollbar-highlight-color:#fff;
+    scrollbar-shadow-color:#eeeeee; 
+    scrollbar-3dlight-color:#eeeeee;
+    scrollbar-arrow-color:#000; 
+    scrollbar-track-color:#fff;
+    scrollbar-darkshadow-color:#fff;
+  }  
+.wrapper{
+    flex-direction: column;
+    justify-content: center;
+}
+.weex{
+  font-size: 60px;
+  text-align: center;
+  color: red;
+}
+.vue{
+  font-size: 60px;
+  text-align: center;
+  margin-top:30px;
+  color: #ccc;
+}
+</style>
+<script>
+export default {
+  name: 'foo',
+  data () {
+    return {
+      msg: 'foo'
+    }
+  }
+}
+</script>
+</script>
